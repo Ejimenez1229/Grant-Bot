22ceb01b26f821343c1e4fe6454b0be8973c7ff7
Ejimenez1229 <elizabethlorelei1229@gmail.com>
2024-07-01 20:58:22 -0700

    Update Data Sorter.py
    Updated to fix bugs

--- Data Sorter.py
+++ Data Sorter.py
@@ -1,70 +1,67 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time 
+import os
+import pandas as pd
 
-###################################################################################################################################################################
-## This the list of key words that the bot will search for, if you ever need to add more just add to the list
-Search_words = ['Tester','Study']
-
-## This is the minimum number of days from today that the grant will end that the bot is looking for, ex 30 means 30 days from today the grant will end 
-Days_til = 90 
-
-## This is the maximum number of days from the day the grant was posted (Added this incase you dont want old grants) Ex 365 means posted within the year 
-Days_From = 365
-####################################################################################################################################################################
-
-##This is the website we are searching in 
-for i in Search_words:
-    service = Service(executable_path='chromedriver.exe')
-    driver = webdriver.Chrome(service=service)
-    driver.get('https://www.grants.gov/search-grants')
-
-    ## Here we are waiting for the elements to show up, website to load 
-    WebDriverWait(driver, 15).until(
-        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="inp-keywords"]'))
-    )
-    input_element = driver.find_element(By.XPATH, '//*[@id="inp-keywords"]')
-    time.sleep(10)
-    input_element.clear()
-    ## input_element.send_keys("Test it out!" + Keys.ENTER)
-    time.sleep(15)
-    input_element.send_keys(i)
-
-    ## Here I am only wanting active so I am unclicking 'Forecast'
-    posted_element = driver.find_element(By.XPATH, '//*[@id="__nuxt"]/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div/label')
-    posted_element.click()
+Search_words = ["Water","Study","more","four","five"]
+words_totals = len(Search_words)
 
 
-    ## Here I am only looking for Grants so I am only clicking Grants
-    WebDriverWait(driver, 15).until(
-        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="m-a1"]/div/div[2]/div[2]/label'))
-    )
-    Grant_element = driver.find_element(By.XPATH, '//*[@id="m-a1"]/div/div[2]/div[2]/label')
-    Grant_element.click()
+## This section I am taking the amount of search words there are and creating a list of dataframes and their names 
+## Data Frame lists to fill in 
+DF_list = []
+DF_list2 = [] 
+DF_list5 = []
+## here I want for every search word ad a new variable 
+for words_total in range(0, words_totals):
+   dataframes_list = ("df"+ str(words_total))
+   DF_list.append(dataframes_list)
+   
+## here for every word attach the appropriate script to run it 
+for i in DF_list:
+    dir_loc = ("C:"+ "\\Users\\eliza\\Downloads\\")
+    DF_list2.append(dir_loc)
 
-    time.sleep(2) 
-
-    ## Push the search button 
-    WebDriverWait(driver, 15).until(
-        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="btn-search"]'))
-    )
-    Push_element = driver.find_element(By.XPATH, '//*[@id="btn-search"]')
-    Push_element.click()
-
-    time.sleep(5) 
+for q in range(0,words_totals):
+    paranthesis = ""
+    DF_list5.append(paranthesis)
 
 
-    ## I am downloading the CSV 
-    WebDriverWait(driver, 15).until(
-        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="__nuxt"]/div[4]/div/div/div/div[2]/div[1]/div[2]/a'))
-    )
-    Dload_element = driver.find_element(By.XPATH, '//*[@id="__nuxt"]/div[4]/div/div/div/div[2]/div[1]/div[2]/a')
-    Dload_element.click()
+## here I will append the names of the documents found in the download folder 
+## I pull the list and append to another list 
+grant_list = []
+grant_list_updated = []
+Grants_combined = []
 
-    time.sleep(10)
-    ## driver.quit()
-    time.sleep(30)
+for i in os.listdir(r'C:\Users\eliza\Downloads'):
+   grant_list.append(i)
+
+grants_list = filter(lambda grant: "grant" in grant, grant_list)
+
+new_list = (list(grants_list))
+## This is the list of grant documents in the download folder 
+grant_list_updated.append(new_list)
+
+
+## NOW I attach list 2 and 3 by their indexes using a zipper 
+DF_list4 = ["".join(pair) for pair in zip(DF_list2,new_list, DF_list5)]
+DF_list4
+
+for i in DF_list4:
+    DF_list2= (pd.read_csv(i))
+    DF_list2.insert(0,"Dummy",1)
+     # some code
+    Grants_combined.append(DF_list2)
+
+b = pd.concat(Grants_combined, ignore_index=True)
+    
+
+Totals = b['Dummy'].sum()
+
+Matrix_DF3 = (b.groupby('OPPORTUNITY TITLE')['Dummy'].sum()/words_totals)*100
+print(Matrix_DF3)
+
+
+
+
+
+
+    
